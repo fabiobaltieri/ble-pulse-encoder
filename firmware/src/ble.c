17d64bfe25f7cb08853999c43eabb0b66ff5f330
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-01-26 19:17:26 +0000

    fw: implement unpairing

--- firmware/src/ble.c
+++ firmware/src/ble.c
@@ -1,10 +1,13 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/init.h>
+#include <zephyr/input/input.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/settings/settings.h>
 #include <zephyr/shell/shell.h>
+
+#include "blinker.h"
 
 LOG_MODULE_REGISTER(ble, LOG_LEVEL_INF);
 
@@ -45,6 +48,40 @@ static int cmd_ble_unpair(const struct shell *sh, size_t argc, char **argv)
 SHELL_CMD_REGISTER(ble_unpair, NULL, "BLE Unpair", cmd_ble_unpair);
 
 #endif
+
+#define UNPAIR_TIMEOUT_S 5
+
+static void unpair_handler(struct k_work *work)
+{
+	bt_unpair(BT_ID_DEFAULT, NULL);
+
+	blink(BLINK_UNPAIRED);
+}
+
+K_WORK_DELAYABLE_DEFINE(unpair_dwork, unpair_handler);
+
+static void unpair_cb(struct input_event *evt)
+{
+	static bool a, b;
+
+        if (evt->type != INPUT_EV_KEY) {
+		return;
+	}
+
+	if (evt->code == INPUT_BTN_LEFT) {
+		a = evt->value;
+	}
+	else if (evt->code == INPUT_BTN_RIGHT) {
+		b = evt->value;
+	}
+
+	if (a && b) {
+		k_work_schedule(&unpair_dwork, K_SECONDS(UNPAIR_TIMEOUT_S));
+	} else {
+		k_work_cancel_delayable(&unpair_dwork);
+	}
+}
+INPUT_CALLBACK_DEFINE(NULL, unpair_cb);
 
 static int ble_setup(void)
 {
